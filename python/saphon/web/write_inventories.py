efbdb738a128204b3d22f7ecf8cb99ffd8f2a8af
whdc <wchang@berkeley.edu>
2015-07-30 18:24:18 -0700

    inventories, done but not tested

--- python/saphon/web/write_inventories.py
+++ python/saphon/web/write_inventories.py
@@ -27,120 +27,82 @@ def ANY (seq, pred=indic): return count(seq, pred) >= 1
 def MANY(seq, pred=indic): return count(seq, pred) >= 2
 def ALL (seq, pred=indic): return count(seq, pred) == len(seq)
 
-def writeInventory(
-  loc,
-  sounds,
-  write: String => Unit,
-  lump: Boolean = false,
-  assemble: (Seq[String] => String) = _.mkString("&nbsp;"),
-  assembleTrans: (Seq[String] => String) = _.mkString("&nbsp;"),
-  table: Boolean = false)
-{
-  # Partition sounds into the below 3 data structures.
-  c = defaultdict(list) # consonant layout
-  v = defaultdict(list) # vowel layout
-  nonsounds = [] # non-sound features
+# Write a table labeled `name` with `sounds`, using `optimizeLayout`
+# to improve the initial layout derived from soundInfo, using
+# `formatLabel` to format labels and `formatSounds` to format lists
+# of sounds, and using `write` to write.
 
+def writeTable(name, sounds, optimizeLayout, formatLabel, formatSounds, write):
+
+  # Create initial layout
+  layout = defaultdict(list)
   for sound in sounds:
     info = soundInfo[sound]
-    if info[0] == 'c':
-      c[info[1], info[2]].append(sound)
-    elif info[0] == 'v':
-      v[info[1], info[2]].append(sound)
-    elif info[0] == 's':
-      nonsounds.append(sound)
+    layout[info[1], info[2]].append(sound)
 
-  # Improve layouts.
-  mannerLabels, placeLabels    = layoutConsonants(c, lump)
-  heightLabels, backnessLabels = layoutVowels    (v, lump)
+  # Improve layouts, get relevant table rows/columns.
+  rowLabels, colLabels = layoutOptimizer(layout)
 
-  # consonant table
-  write( "<div class=field><table class=inv>\n")
+  # Write out layout
+  write('<div class=field><table class=inv>\n')
 
-  for( i <- '?' +: mannerC if i == '?' || mannerS( i) != null) {
-    write( "<tr>")
-    if( i != '?') { # manner headers
-      write( "<td class=header>")
-      write( Xlt(loc, mannerS( i))))
-      write( "</td>")
-    } else {
-      write( "<td class=key>")
-      write( Xlt(loc, "consonants")))
-      write( "</td>")
-    }
+  for i in ['?'] + rowLabels.keys():
+    write('<tr>')
+    if i == '?': # top-left cell
+      write('<td class=key>')
+      write(formatLabel(name))
+      write('</td>')
+    else: # row header
+      write('<td class=header>')
+      write(formatLabel(rowLabels(i)))
+      write('</td>')
 
-    for( j <- placeC if placeS( j) != null) {
-      if( i == '?') { # place headers
-        write( "<td class=header>")
-        write( Xlt(loc, placeS( j))))
-        write( "</td>")
-      } else { # body cells
-        write( "<td>")
-        write( assemble( c((i,j)).toSeq.sortBy( soundOrderMap( _))))
-        write( "</td>")
-      }
-    }
-    write( "</tr>\n")
-  }
-  write( "</table></div>\n")
+    for j in colLabels.keys():
+      if i == '?': # column header
+        write('<td class=header>')
+        write(formatLabel(colLabels(j)))
+        write('</td>')
+      else: # body cells
+        write('<td>')
+        write(formatSounds(layout[i,j]))
+        write('</td>')
+    write('</tr>\n')
 
-  # vowel table
-  write( "<div class=field><table class=inv>\n")
+  write('</table></div>\n')
 
-  for( i <- '?' +: heightC if i == '?' || heightS( i) != null) {
-    write( "<tr>")
-    if( i != '?') { # height headers
-      write( "<td class=header>")
-      write( Xlt(loc, heightS( i))))
-      write( "</td>")
-    } else {
-      write( "<td class=key>")
-      write( Xlt(loc, "vowels")))
-      write( "</td>")
-    }
+# Write a field labeled `name` with `nonsounds`, using `formatLabel`
+# to format labels and `formatSounds` to format lists of sounds, and
+# using `writeField` to write.
 
-    for( j <- backnessC if backnessS( j) != null) {
-      if( i == '?') { # backness headers
-        write( "<td class=header>")
-        write( Xlt(loc, backnessS( j))))
-        write( "</td>")
-      } else { # body cells
-        write( "<td>")
-        write( assemble( v((i,j)).toSeq.sortBy( soundOrderMap( _))))
-        write( "</td>")
-      }
-    }
-    write( "</tr>\n")
-  }
-  write( "</table></div>\n")
+def writeNonsounds(name, nonsounds, formatLabel, formatNonsounds, writeField):
+  writeField(formatLabel(name), formatNonsounds(nonsounds))
 
-  if( table) {
-    write( "<div class=field>\n")
-    write( "<div class=key>" 
-      ++ Xlt(loc, "suprasegmental")) 
-      ++ ":</div>\n")
-    write( "<div class=value>")
-    write( assembleTrans( ss_))
-    write( "</div></div>\n")
-  } else if( !ss_.isEmpty) {
-    write( "<div class=field><div class=key>" 
-      ++ Xlt(loc, "suprasegmental")) 
-      ++ "</div><div class=value>")
-    write( cap( ss_.map( xlt(loc, _)).mkString( ", ")))
-    write( "</div></div>\n")
-  }
-}
-
-inventoryHead = """
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-<link rel="stylesheet" type="text/css" href="../../inv.css" />
-</head>
-<body>
-"""
+#  if( table) {
+#    write( "<div class=field>\n")
+#    write( "<div class=key>" 
+#      ++ Xlt(loc, "suprasegmental")) 
+#      ++ ":</div>\n")
+#    write( "<div class=value>")
+#    write( assembleTrans( ss_))
+#    write( "</div></div>\n")
+#  } else if( !ss_.isEmpty) {
+#    write( "<div class=field><div class=key>" 
+#      ++ Xlt(loc, "suprasegmental")) 
+#      ++ "</div><div class=value>")
+#    write( cap( ss_.map( xlt(loc, _)).mkString( ", ")))
+#    write( "</div></div>\n")
+#  }
 
 def writeLocal(saphonData, htmlDir, loc):
   metalang = loc.metalang_code
+
+  inventoryHead = """
+  <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <link rel="stylesheet" type="text/css" href="../../inv.css" />
+  </head>
+  <body>
+  """
 
   # Create a file for our own use that contains all inventories.
   foMaster = open(htmlDir+'/'+metalang+'/inv/master.html', 'w')
@@ -175,8 +137,29 @@ def writeLocal(saphonData, htmlDir, loc):
 
     writeField('family', lang.familyStr)
 
-    writeInventory(loc, lang.feat_, write)
+    writeTable(
+      'consonants',
+      [s for s in sounds if soundInfo[s][0] == 'c'],
+      lambda layout: optimizeConsonantLayout(layout, lump),
+      lambda label: Xlt(loc, label),
+      lambda sounds: '&nbsp'.join(sounds),
+      write)
 
+    writeTable(
+      'vowels',
+      [s for s in sounds if soundInfo[s][0] == 'v'],
+      lambda layout: optimizeVowelLayout(layout, lump),
+      lambda label: Xlt(loc, label),
+      lambda sounds: '&nbsp'.join(sounds),
+      write)
+
+    writeNonsounds(
+      'suprasegmentals',
+      [s for s in sounds if soundInfo[s][0] == 's'],
+      lambda label: Xlt(loc, label),
+      lambda sounds: ', '.join(xlt(loc, sounds)).capitalize(),
+      writeField)
+      
     if lang.bib_:
       bibStr = ''.join('<p>'+bib+'</p>\n' for bib in lang.bib_)
       writeField('bibliography', bibStr)
