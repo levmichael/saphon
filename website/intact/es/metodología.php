3008ee947c6a1fc7fe4b121b84fa3fa7107c34df
Emily Clem <emily.c.clem@gmail.com>
2015-12-17 17:35:22 -0800

    split Ticuna dialects

---
+++ website/intact/es/metodología.php
@@ -0,0 +1,22 @@
+<?php include("header-title.php"); ?>
+<?php include("nav-about.php"); ?>
+
+<div id="content">
+<h5>Metodología</h5>
+
+Los inventarios fonológicos de SAPhon fueron recopilados minando datos de publicaciones existentes o de inventarios inéditos de lingüistas que han trabajado en las lenguas pertinentes. A pesar de la aparente sencillez de esta tarea, hay varios asuntos que pueden generar algunas dificultades. Nuestra meta aquí es describir las complicaciones y explicar como las enfrentamos ya que que algunas decisiones conllevan determinaciones que podrían afectar la corrección de la base de datos. Queremos alentar a aquellos especialistas que detecten errores en la base de datos a que nos contacten para proponer <a href="contribuya.php">correcciones</a>.
+</p>
+
+Primero, es posible que varios inventarios fonológicos para la misma lengua hayan sido sugeridos en distintas publicaciones. Dado a que en muchos casos no somos especialistas en un lenguaje particular, o en una familia lingüíistica, se nos hace imposible discernir entre análisis conflictivos; a la hora de seleccionar el inventario mas fiable, debemos apoyarnos en varios ‘proxies’. Algunos proxies básicos son: 1) El nivel de entrenamiento lingüístico del autor del inventario; 2) contundencia de la evidencia lingüística y de la discusión que acompaña el inventario; 3) la verosimilitud tipológica y la consistencia del inventario con otros inventarios de lenguas relacionadas (si éstas existen); y 4) la vigencia de la publicación. Obviamente, ninguno de estos criterios es infalible, lo cual hace aún mas apremiante que investigadores de lenguas particulares y especialistas en las familias lingüísticas relevantes <a href="contribuya.php">contribuyan</a> a este proyecto.
+</p>
+
+Segundo, pueden encontrarse ambigüedades en la descripción o el análisis de los inventarios fonológicos. En algunos casos, por ejemplo, el símbolo usado para representar un fonema o la descripción en prosa de los rasgos fonológicos del fonema pueden ser inconsistentes con el uso moderno del mismo símbolo. En otros casos raros, pueden existir errores en la argumentación fonológica que apoya un inventario determinado, lo cual sugeriría que algunos segmentos deberían ser añadidos o removidos del inventario en cuestión.
+</p>
+
+Es importante subrayar que no intentamos crear inventarios fonológicos para casos en que no se ha llevado a cabo ningún análisis fonológico. Hay lenguas en las que existen datos léxicos, por ejemplo listas del siglo 19 con palabras recopiladas por naturistas, etnógrafos, misioneros, o exploradores, pero sin embargo estas listas no están incluidas en la base de datos de SAPhon porque estas palabras no han sido utilizadas en ningún análisis fonológico. En la base de datos incluímos datos de trabajos filológicos que han propuesto inventarios fonológicos. 
+</p>
+</div>		
+</div>
+
+</body>
+</html>
