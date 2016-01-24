506c65fffc5f6800dbb7ef208d501c2e184ff355
Emily Clem <emilyclem@users.noreply.github.com>
2016-01-24 15:49:31 -0800

    Change version number

--- python/saphon/web/write_saphon_php.py
+++ python/saphon/web/write_saphon_php.py
@@ -1,7 +1,7 @@
 def write(saphonData, htmlDir):
   fo = open(htmlDir + '/saphon.php', 'w')
   fo.write("""<?php
-$version = "1.1.3";
+$version = "1.1.4";
 $n_lang = "%d";
 ?>""" % len(saphonData.lang_))
   fo.close()
