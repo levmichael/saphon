12caa7ada73ce0c844b72cad1bceb28f04199aef
levmichael <levmichael@berkeley.edu>
2020-07-28 12:35:41 -0700

    Update write_saphon_php.py

--- python/saphon/web/write_saphon_php.py
+++ python/saphon/web/write_saphon_php.py
@@ -1,7 +1,7 @@
 def write(saphonData, htmlDir):
   fo = open(htmlDir + '/saphon.php', 'w')
   fo.write("""<?php
-$version = "1.1.5";
+$version = "2.0.0";
 $n_lang = "%d";
 ?>""" % len(saphonData.lang_))
   fo.close()
