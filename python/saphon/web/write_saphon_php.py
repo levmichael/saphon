9b662d63f7f8b40488f3cab3c901a27d0cfe67ae
allegrarobertson <78765286+allegrarobertson@users.noreply.github.com>
2021-08-23 16:43:02 -0700

    Update write_saphon_php.py

--- python/saphon/web/write_saphon_php.py
+++ python/saphon/web/write_saphon_php.py
@@ -1,7 +1,7 @@
 def write(saphonData, htmlDir):
   fo = open(htmlDir + '/saphon.php', 'w')
   fo.write("""<?php
-$version = "2.0.0";
+$version = "2.1.0";
 $n_lang = "%d";
 ?>""" % len(saphonData.lang_))
   fo.close()
